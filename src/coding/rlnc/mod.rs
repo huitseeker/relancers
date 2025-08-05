688322febc5d5bcd1303c31dc9c3bb51c62275e9
François Garillot <4142+huitseeker@users.noreply.github.com>
2025-08-05 11:10:38 -0400

    feat: simpler seeded encoding (#7)

--- src/coding/rlnc/mod.rs
+++ src/coding/rlnc/mod.rs
@@ -3,9 +3,7 @@
 mod decoder;
 mod encoder;
 mod optimized_matrix;
-mod seed_encoder;
 
 pub use decoder::RlnDecoder;
 pub use encoder::RlnEncoder;
 pub use optimized_matrix::OptimizedMatrix;
-pub use seed_encoder::SeedRlnEncoder;
