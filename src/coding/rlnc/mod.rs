57bb563f65a002c961a1505835b2b787dbcb1564
François Garillot <francois@garillot.net>
2025-08-04 21:49:37 -0400

    simpler sparsity

--- src/coding/rlnc/mod.rs
+++ src/coding/rlnc/mod.rs
@@ -4,10 +4,8 @@ mod decoder;
 mod encoder;
 mod optimized_matrix;
 mod seed_encoder;
-mod sparse_encoder;
 
 pub use decoder::RlnDecoder;
 pub use encoder::RlnEncoder;
 pub use optimized_matrix::OptimizedMatrix;
 pub use seed_encoder::SeedRlnEncoder;
-pub use sparse_encoder::SparseRlnEncoder;
